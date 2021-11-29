db2a6c6e6bbd1bdb4638c608c8105b1cf6fd46aa
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-11-29 08:21:18 -0800

    Challenge

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -118,8 +118,16 @@ This function should use forEach to populate your grocery list based on the stor
 ------------------------------------------------------------------------------------------------ */
 
 const createList = (availableItems) => {
-  // Solution code here...
-};
+  const newArray = [];
+
+   availableItems.forEach(item => {
+     if (item.available === true){
+       newArray.push(item.name);
+     }
+   
+    });
+   return newArray;
+    };
 
 /* ------------------------------------------------------------------------------------------------
 STRETCH - CHALLENGE 7
